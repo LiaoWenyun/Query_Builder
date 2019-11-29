83c5840b5d79feaecf23a790477010648925a789
LiaoWenyun <xiliao2@hotmail.com>
2019-11-28 16:56:59 -0800

    started the new notebook

---
+++ query_builder/final_core.py
@@ -0,0 +1,28 @@
+import pandas
+import ipywidgets as widgets
+import pyvo
+from IPython.display import Image, display, clear_output
+
+__all__ = ['QueryBuilder']
+
+class QueryBuilder:
+    def __init__(self):
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    def Start_query(self):
+        
+    
+    
+    
+    
+    
+    
+    def __get_service(self):
+        
